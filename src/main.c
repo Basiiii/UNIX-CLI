862ba1119ec2fe6b58961ebebd25f48bb3c1e1f6
Enrique R. (Basi) <hello.basi@proton.me>
2024-03-22 13:31:29 +0000

    Implement List Directory
    Implements list directory functionality, which is functionality 1.g.

--- src/main.c
+++ src/main.c
@@ -4,10 +4,9 @@
  * @brief  This file contains the main entry point of the program.
  *
  * The program is a command-line utility that provides various functionalities
- * for file manipulation and filesystem management. It includes commands for
- * displaying file contents, copying files, appending file contents, counting
- * lines in files, deleting files, displaying filesystem information, and
- * listing directories.
+ * for file manipulation. It includes commands for displaying file contents,
+ * copying files, appending file contents, counting lines in files, deleting
+ * files, displaying filesystem information, and listing directories.
  *
  * The program also includes a custom command interpreter
  * for executing these commands. Each command is implemented using system calls
@@ -33,60 +32,17 @@
  *
  * The main function serves as the entry point of the program. It executes the
  * command-line utility, which provides various functionalities for file
- * manipulation and filesystem management. This includes commands for displaying
- * file contents, copying files, appending file contents, counting lines in
- * files, deleting files, displaying filesystem information, and listing
- * directories. Additionally, the program includes a custom command interpreter
- * for executing these commands. Each command is implemented using system calls
- * to perform low-level file and filesystem operations.
+ * manipulation. This includes commands for displaying file contents, copying
+ * files, appending file contents, counting lines in files, deleting files,
+ * displaying filesystem information, and listing directories. Additionally, the
+ * program includes a custom command interpreter for executing these commands.
+ * Each command is implemented using system calls to perform low-level file and
+ * filesystem operations.
  *
  * @return int Returns 0 upon successful execution.
  */
 int main() {
   printf("%s version %s.\n\n", PROGRAM_NAME, VERSION);
-
-  // int result = ShowFile("test");
-  // if (result != SUCCESS) {
-  //   perror("Error");
-  // }
-
-  // int result1 = CopyFile("test");
-  // if (result1 != SUCCESS) {
-  //   perror("Error");
-  // }
-
-  // int result2 = AppendFile("source", "dest");
-  // if (result2 != SUCCESS) {
-  //   perror("Error");
-  // }
-
-  // int numLines = 0;
-  // int result3 = CountLines("test", &numLines);
-  // if (result3 < SUCCESS) {
-  //   PrintError(result3);
-  // } else if (result3 > SUCCESS) {
-  //   perror("Error");
-  // }
-  // printf("Number of lines: %d\n", numLines);
-
-  // int result4 = DeleteFile("test");
-  // if (result4 != SUCCESS) {
-  //   perror("Error");
-  // }
-
-  // FileInfo *info = GetFileInfo("test");
-  // if (info == NULL) {
-  //   fprintf(stderr, "Error retrieving file information.\n");
-  //   return 1;
-  // }
-  // printf("File type: %s\n", info->fileType);
-  // printf("Owner: %s\n", info->owner);
-  // printf("Creation time: %s", info->creationTime);
-  // printf("Last access time: %s", info->lastAccessTime);
-  // printf("Last modification time: %s", info->lastModificationTime);
-  // printf("Inode: %ld\n", info->inode);
-
-  // free(info);
 
   return 0;
 }
